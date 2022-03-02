c3e3d687bbd1504ecdc11a5b299752fbf603f70f
Pierre Sevenig <pierre_sevenig@live.com>
2022-03-02 17:33:04 +0100

    Frontend developped

--- app.py
+++ app.py
@@ -2,22 +2,37 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from countries import countries
+import requests
+import folium
+from streamlit_folium import folium_static
+from folium import plugins
 
-#Webpage
-st.set_page_config(layout="wide")
+#Basic Layout stuff
+st.set_page_config(layout='wide')
 
+col1, col2 = st.columns(2)
+
+#Get dropdown list for countries
 def get_country():
-    return pd.DataFrame(['Afghanistan', 'Aland Islands', 'Albania', 'Algeria', 'American Samoa', 'Andorra', 'Angola', 'Anguilla', 'Antarctica', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia, Plurinational State of', 'Bonaire, Sint Eustatius and Saba', 'Bosnia and Herzegovina', 'Botswana', 'Bouvet Island', 'Brazil', 'British Indian Ocean Territory', 'Brunei Darussalam', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Cayman Islands', 'Central African Republic', 'Chad', 'Chile', 'China', 'Christmas Island', 'Cocos (Keeling) Islands', 'Colombia', 'Comoros', 'Congo', 'Congo, The Democratic Republic of the', 'Cook Islands', 'Costa Rica', "Côte d'Ivoire", 'Croatia', 'Cuba', 'Curaçao', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia', 'Falkland Islands (Malvinas)', 'Faroe Islands', 'Fiji', 'Finland', 'France', 'French Guiana', 'French Polynesia', 'French Southern Territories', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guadeloupe', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Heard Island and McDonald Islands', 'Holy See (Vatican City State)', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran, Islamic Republic of', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', "Korea, Democratic People's Republic of", 'Korea, Republic of', 'Kuwait', 'Kyrgyzstan', "Lao People's Democratic Republic", 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macao', 'Macedonia, Republic of', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Martinique', 'Mauritania', 'Mauritius', 'Mayotte', 'Mexico', 'Micronesia, Federated States of', 'Moldova, Republic of', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Myanmar', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Niue', 'Norfolk Island', 'Northern Mariana Islands', 'Norway', 'Oman', 'Pakistan', 'Palau', 'Palestinian Territory, Occupied', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Pitcairn', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Réunion', 'Romania', 'Russian Federation', 'Rwanda', 'Saint Barthélemy', 'Saint Helena, Ascension and Tristan da Cunha', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Martin (French part)', 'Saint Pierre and Miquelon', 'Saint Vincent and the Grenadines', 'Samoa', 'San Marino', 'Sao Tome and Principe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Sint Maarten (Dutch part)', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'South Georgia and the South Sandwich Islands', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'South Sudan', 'Svalbard and Jan Mayen', 'Swaziland', 'Sweden', 'Switzerland', 'Syrian Arab Republic', 'Taiwan, Province of China', 'Tajikistan', 'Tanzania, United Republic of', 'Thailand', 'Timor-Leste', 'Togo', 'Tokelau', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Turks and Caicos Islands', 'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'United States Minor Outlying Islands', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Venezuela, Bolivarian Republic of', 'Viet Nam', 'Virgin Islands, British', 'Virgin Islands, U.S.', 'Wallis and Futuna', 'Yemen', 'Zambia', 'Zimbabwe'])
+    return countries
 
-country=get_country()
+country = get_country()
 
-country = st.selectbox('Select a country', country)
+with col1:
+    st.multiselect('Select countries', country)
 
-number = st.number_input('Select a number of clusters', 1, 10000)
+#Select number of clusters
+with col2:
+    st.number_input('Select a number of clusters', 1, 10000)
 
-def get_map_data():
-    return pd.DataFrame(
-        [[51.14365216, 12.90129043],
+#select percentage of people to reach
+st.slider('Percenage of people to reach',0, 100)
+
+
+
+#data for coordinates
+coordinate = [[51.14365216, 12.90129043],
        [49.9082329 ,  8.32919233],
        [48.51070336,  9.05212022],
        [49.95573197, 10.91326732],
@@ -26,9 +41,118 @@ def get_map_data():
        [51.94432852, 10.19479602],
        [52.47122199,  8.2674454 ],
        [52.72249955, 13.29947277],
-       [48.34339277, 11.74843626]],
-        columns=['lat', 'lon'])
+       [48.34339277, 11.74843626]]
 
-df = get_map_data()
+#Get Dataframe
+url = 'https://nominatim.openstreetmap.org/reverse'
+responses=[]
+for coordinates in coordinate:
 
-st.map(df)
+      lat = coordinates[0]
+      lon = coordinates[1]
+      params = {
+          'lat': lat,
+          'lon': lon,
+          'format': 'json'
+      }
+      response = requests.get(url, params = params).json()
+      responses.append(response['address'])
+df = pd.DataFrame(responses)
+df = df[['road', 'village', 'town', 'county', 'state', 'postcode', 'country']]
+df = df.replace(np.nan, "Not Available")
+
+#center the map
+center =[sum(i[0] for i in coordinate)/len(coordinate), sum(i[1] for i in coordinate)/len(coordinate)]
+
+
+#display map
+m = folium.Map(location=center,
+
+                )
+
+
+#display coordinates on map and style tooltip
+for i in range(len(df)):
+  folium.Marker(coordinate[i], tooltip=f"""Road: {df.loc[i,'road']},
+                                        Village: {df.loc[i,'village']},
+                                        Town: {df.loc[i,'town']},
+                                        County: {df.loc[i, 'county']}""").add_to(m)
+
+
+
+#Google Maps
+basemaps = {
+    'Google Maps': folium.TileLayer(
+        tiles = 'https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+        attr = 'Google',
+        name = 'Google Maps',
+        overlay = True,
+        control = True
+    ),
+    'Google Satellite': folium.TileLayer(
+        tiles = 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
+        attr = 'Google',
+        name = 'Google Satellite',
+        overlay = True,
+        control = True
+    ),
+    'Google Terrain': folium.TileLayer(
+        tiles = 'https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',
+        attr = 'Google',
+        name = 'Google Terrain',
+        overlay = True,
+        control = True
+    ),
+    'Google Satellite Hybrid': folium.TileLayer(
+        tiles = 'https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',
+        attr = 'Google',
+        name = 'Google Satellite',
+        overlay = True,
+        control = True
+    ),
+    'Esri Satellite': folium.TileLayer(
+        tiles = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        attr = 'Esri',
+        name = 'Esri Satellite',
+        overlay = True,
+        control = True
+    )
+}
+
+# Add custom basemaps
+
+basemaps['Google Maps'].add_to(m)
+basemaps['Google Satellite Hybrid'].add_to(m)
+basemaps['Esri Satellite'].add_to(m)
+folium.LayerControl().add_to(m)
+
+
+
+
+#Plugins
+
+plugins.Fullscreen().add_to(m)
+plugins.LocateControl().add_to(m)
+plugins.MeasureControl(position='topright', primary_length_unit='kilometers', secondary_length_unit='miles', primary_area_unit='sqmeters', secondary_area_unit='acres').add_to(m)
+minimap = plugins.MiniMap()
+m.add_child(minimap)
+
+#####
+
+#automatic zoom start
+
+sw = min(coordinate)[0], min(coordinate)[1]
+ne = max(coordinate)[0], max(coordinate)[1]
+
+m.fit_bounds([sw, ne])
+
+# call to render Folium map in Streamlit
+
+
+folium_static(m)
+
+
+#Get Address to display on page
+
+
+st.dataframe(df)
