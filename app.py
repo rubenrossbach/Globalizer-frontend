1ce96ce85cebdabfb77343880c28fd98346fa970
Ruben Rossbach <ruben.rossbach@gmx.net>
2022-03-08 10:56:37 +0100

    fixed errors, display map on startup

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 import app1
 import app2
 import streamlit as st
-st.set_page_config(layout='wide')
+
+st.set_page_config(
+    page_title="Globalizer",
+    page_icon="",
+    layout="wide",
+    initial_sidebar_state="collapsed")
+
+CSS = """
+iframe {
+    width: 100%;
+    height: 700px;
+}
+"""
+
+st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
+
+#st.set_page_config(layout='wide')
 PAGES = {
 "Display centers on a map": app1,
 "Statistics about the country": app2
--- app1.py
+++ app1.py
@@ -12,11 +12,23 @@ from folium.plugins import HeatMapWithTime
 
 
 def app():
-#Basic Layout stuff
+    st.markdown(
+        "<h1 style='text-align: center; color: black;'>Globalizer</h1>",
+        unsafe_allow_html=True)
+
+
+    st.write(
+        """
+        Find the optimal locations for you warehouses, data centers or offices.
+        Simply enter a country and let us do the magic.
+        """
+    )
+    #Basic Layout stuff
 
     col1, col2 = st.columns(2)
 
-#Get dropdown list for countries
+
+    #Get dropdown list for countries
     def get_country():
         return countries
 
@@ -25,7 +37,7 @@ def app():
 
     country_list = st.multiselect('Select countries', country)
 
-#Select buttons for mean distance/number of clusters
+    #Select buttons for mean distance/number of clusters
 
     if "current" not in st.session_state:
 
@@ -71,7 +83,7 @@ def app():
 
             st.session_state.AAA = False
 
-            threshold = st.slider('Select mean distance to coustomer',10, 500)
+            threshold = st.slider('Select mean distance to coustomer in km',10, 500)
             centers = None
 
 
@@ -79,147 +91,199 @@ def app():
     country_code_list = [trans[c] for c in country_list]
     country_code_string = ",".join(country_code_list)
 
-#get coordinates
-    base_url = 'https://globalizer-6mlwsep2ja-ey.a.run.app/predict/'
-    params = {
-            'country': country_code_string,
-            'threshold': threshold,
-            'n_centers': centers
-        }
-    response1 = requests.get(base_url, params = params)
-    st.write(response1.status_code)
-    if response1.status_code == 500:
-        coordinate = [
-       [48.51070336,  9.05212022],
-       [49.95573197, 10.91326732],
-       [53.75704471, 10.06461158],
-       [51.17749919,  6.9799287 ],
-       [51.94432852, 10.19479602],
-       [52.47122199,  8.2674454 ],
-       [52.72249955, 13.29947277],
-       [48.34339277, 11.74843626]]
-        st.write('Internal server erros, using hardcoded coordinates instead.')
+    # button to run
+    col1, col2, col3 , col4, col5 = st.columns(5)
 
-    else:
-        #response1 = response1.json()
-        coordinate = response1.json()["centers"]
-
-#Get Dataframe
-    url = 'https://nominatim.openstreetmap.org/reverse'
-    responses=[]
-    for coordinates in coordinate:
-
-        lat = coordinates[0]
-        lon = coordinates[1]
-        params = {
-                'lat': lat,
-                'lon': lon,
-                'format': 'json'
-            }
-        response = requests.get(url, params = params).json()
-        responses.append(response['display_name'])
-    df = pd.DataFrame({
-        "Address": responses,
-        "Coordinates": coordinate
-    })
-
-#center the map
-    center =[sum(i[0] for i in coordinate)/len(coordinate), sum(i[1] for i in coordinate)/len(coordinate)]
-
-
-#display map
-    col1, col2 = st.columns(2)
     with col1:
-        m = folium.Map(location=center)
+        pass
+    with col2:
+        pass
+    with col4:
+        pass
+    with col5:
+        pass
+    with col3 :
+        run_button = False
+        if (st.session_state.current == "A" or st.session_state.current == "B") \
+            and country_code_list != []:
+            run_button = st.button('Run')
 
 
-#display coordinates on map and style tooltip
-    for i in range(len(df)):
-        folium.Marker(coordinate[i], tooltip=df.loc[i,'Address']).add_to(m)
+    if run_button:
+
+        #get coordinates
+        base_url = 'https://globalizer-2chu5w4mva-ey.a.run.app/predict'
+        params = {
+                'country': country_code_string,
+                'threshold': threshold,
+                'n_centers': centers
+            }
+        response1 = requests.get(base_url, params = params)
+        st.write(response1.status_code)
+        if response1.status_code == 500:
+            coordinate = [
+        [48.51070336,  9.05212022],
+        [49.95573197, 10.91326732],
+        [53.75704471, 10.06461158],
+        [51.17749919,  6.9799287 ],
+        [51.94432852, 10.19479602],
+        [52.47122199,  8.2674454 ],
+        [52.72249955, 13.29947277],
+        [48.34339277, 11.74843626]]
+            st.write('Internal server erros, using hardcoded coordinates instead.')
+
+        else:
+            #response1 = response1.json()
+            coordinate = response1.json()["centers"]
+
+        #Get Dataframe
+        url = 'https://nominatim.openstreetmap.org/reverse'
+        responses=[]
+        for coordinates in coordinate:
+
+            lat = coordinates[0]
+            lon = coordinates[1]
+            params = {
+                    'lat': lat,
+                    'lon': lon,
+                    'format': 'json'
+                }
+            response = requests.get(url, params = params).json()
+            responses.append(response['display_name'])
+        df = pd.DataFrame({
+            "Address": responses,
+            "Coordinates": coordinate
+        })
+
+        #center the map
+        center =[sum(i[0] for i in coordinate)/len(coordinate), sum(i[1] for i in coordinate)/len(coordinate)]
 
 
-#Google Maps
-    basemaps = {
-        'Google Maps': folium.TileLayer(
-            tiles = 'https://mt1.google.com/vt/lyrs=h&x={x}&y={y}&z={z}',
-            attr = 'Google',
-            name = 'Google Maps',
-            overlay = True,
-            control = True
-        ),
-        'Google Satellite': folium.TileLayer(
-            tiles = 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
-            attr = 'Google',
-            name = 'Google Satellite',
-            overlay = True,
-            control = True
-        ),
-        'Google Terrain': folium.TileLayer(
-            tiles = 'https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',
-            attr = 'Google',
-            name = 'Google Terrain',
-            overlay = True,
-            control = True
-        ),
-        'Google Satellite Hybrid': folium.TileLayer(
-            tiles = 'https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',
-            attr = 'Google',
-            name = 'Google Satellite',
-            overlay = True,
-            control = True
-        ),
-        'Esri Satellite': folium.TileLayer(
-            tiles = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-            attr = 'Esri',
-            name = 'Esri Satellite',
-            overlay = True,
-            control = True
-        )
-    }
-
-##Data for heatmap
-#df_heat = df_small.drop(['Centers'], axis=1)
-#df_heat = df_heat.to_numpy()
-#HeatMap(df_heat).add_to(m)
+        #display map
+        col1, col2 = st.columns(2)
+        with col1:
+            m = folium.Map(location=center)
 
 
-# Add custom basemaps
-
-    basemaps['Google Maps'].add_to(m)
-    basemaps['Google Satellite Hybrid'].add_to(m)
-    basemaps['Google Terrain'].add_to(m)
-    folium.LayerControl().add_to(m)
+        #display coordinates on map and style tooltip
+        for i in range(len(df)):
+            folium.Marker(coordinate[i], tooltip=df.loc[i,'Address']).add_to(m)
 
 
+        #Google Maps
+        basemaps = {
+            'Google Maps': folium.TileLayer(
+                tiles = 'https://mt1.google.com/vt/lyrs=h&x={x}&y={y}&z={z}',
+                attr = 'Google',
+                name = 'Google Maps',
+                overlay = True,
+                control = True
+            ),
+            'Google Satellite': folium.TileLayer(
+                tiles = 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
+                attr = 'Google',
+                name = 'Google Satellite',
+                overlay = True,
+                control = True
+            ),
+            'Google Terrain': folium.TileLayer(
+                tiles = 'https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',
+                attr = 'Google',
+                name = 'Google Terrain',
+                overlay = True,
+                control = True
+            ),
+            'Google Satellite Hybrid': folium.TileLayer(
+                tiles = 'https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',
+                attr = 'Google',
+                name = 'Google Satellite',
+                overlay = True,
+                control = True
+            ),
+            'Esri Satellite': folium.TileLayer(
+                tiles = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+                attr = 'Esri',
+                name = 'Esri Satellite',
+                overlay = True,
+                control = True
+            )
+        }
+
+        ##Data for heatmap
+        #df_heat = df_small.drop(['Centers'], axis=1)
+        #df_heat = df_heat.to_numpy()
+        #HeatMap(df_heat).add_to(m)
 
 
-#Plugins
+        # Add custom basemaps
 
-    plugins.Fullscreen().add_to(m)
-    plugins.LocateControl().add_to(m)
-    plugins.MeasureControl(position='topright', primary_length_unit='kilometers', secondary_length_unit='miles', primary_area_unit='sqmeters', secondary_area_unit='acres').add_to(m)
-    minimap = plugins.MiniMap()
-    m.add_child(minimap)
-
-#####
-
-
-#automatic zoom start
-
-    sw = min(coordinate)[0], min(coordinate)[1]
-    ne = max(coordinate)[0], max(coordinate)[1]
-
-    m.fit_bounds([sw, ne])
+        basemaps['Google Maps'].add_to(m)
+        basemaps['Google Satellite Hybrid'].add_to(m)
+        basemaps['Google Terrain'].add_to(m)
+        folium.LayerControl().add_to(m)
 
 
 
 
-# call to render Folium map in Streamlit
+        #Plugins
 
-    folium_static(m)
+        plugins.Fullscreen().add_to(m)
+        plugins.LocateControl().add_to(m)
+        plugins.MeasureControl(position='topright', primary_length_unit='kilometers', secondary_length_unit='miles', primary_area_unit='sqmeters', secondary_area_unit='acres').add_to(m)
+        minimap = plugins.MiniMap()
+        m.add_child(minimap)
+
+        #####
 
 
-#Get Address to display on page
-    st.write("# Your Centers")
-    st.dataframe(df)
+        #automatic zoom start
+
+        sw = min(coordinate)[0], min(coordinate)[1]
+        ne = max(coordinate)[0], max(coordinate)[1]
+
+        m.fit_bounds([sw, ne])
+
+
+
+
+        # call to render Folium map in Streamlit
+
+        folium_static(m)
+
+
+        #Get Address to display on page
+        st.write("# Your Centers")
+        st.dataframe(df)
+
+    # no button pressed
+    else:
+        #Setting up the world countries data URL
+        # url = 'https://raw.githubusercontent.com/python-visualization/folium/master/examples/data'
+        # country_shapes = f'{url}/world-countries.json'
+        # basemaps = {
+        #     'Google Maps': folium.TileLayer(
+        #         tiles = 'https://mt1.google.com/vt/lyrs=h&x={x}&y={y}&z={z}',
+        #         attr = 'Google',
+        #         name = 'Google Maps',
+        #         overlay = True,
+        #         control = True
+        #     )
+        # }
+        m = folium.Map() #location=[0,0], zoom_start=2.5)
+        # folium.Choropleth(
+        # #The GeoJSON data to represent the world country
+        # geo_data=country_shapes,
+        # # name='choropleth COVID-19',
+        # # data=df_covid,
+        # #The column aceppting list with 2 value; The country name and  the numerical value
+        # # columns=['Country', 'Total Case'],
+        # # key_on='feature.properties.name',
+        # fill_color='blue',
+        # # nan_fill_color='white'
+        # ).add_to(m)
+        m.fit_bounds([[-50,  -30],[70,60]])
+        # basemaps['Google Maps'].add_to(m)
+        folium_static(m)
+
+
     return
