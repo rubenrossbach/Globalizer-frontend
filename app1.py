b6c5caf8a4083b850a387d4aa5a3eb4fb6e7b929
Pierre Sevenig <pierre_sevenig@live.com>
2022-03-10 13:47:45 +0100

    presentation_frontend

--- app1.py
+++ app1.py
@@ -9,12 +9,19 @@ import folium
 from streamlit_folium import folium_static
 from folium import plugins
 from country_statistics import show_country_statistics
+#import Image
 
 def app():
 
-    st.markdown(
-        """
-        <h1 style='text-align: center; color: black;'><u>Globalizer</u></h1>
+    title_container = st.container()
+    col1, col2 = st.columns([1, 1000])
+    image = 'https://freesvg.org/img/1386749018.png'
+    with title_container:
+        with col1:
+            st.image(image, width=45)
+        with col2:
+            st.markdown("""
+        <h1 style='text-align: center; color: black;'>Globalizer</h1>
         <p style='text-align: center'>Expanding to a new country and wondering where to open your business's
         locations?</p>
         <p style='text-align: center'>Let us help you with our Machine Learning algorithm based on high-resolution
@@ -22,9 +29,20 @@ def app():
         It makes optimal decisions based on where a country's population is
         actually located. <br>
         Choose a country to get started.</p>
-        """,
-        unsafe_allow_html=True
-    )
+        """,unsafe_allow_html=True)
+    #st.markdown(
+     #   """
+     #   <h1 style='text-align: center; color: black;'><u>Globalizer</u></h1>
+     #   <p style='text-align: center'>Expanding to a new country and wondering where to open your business's
+     #   locations?</p>
+     #   <p style='text-align: center'>Let us help you with our Machine Learning algorithm based on high-resolution
+     #   population density data.
+     #   It makes optimal decisions based on where a country's population is
+     #   actually located. <br>
+     #   Choose a country to get started.</p>
+     #   """,
+     #   unsafe_allow_html=True
+    #)
 
     #Get dropdown list for countries
     def get_country():
@@ -51,14 +69,13 @@ def app():
 
         st.session_state.CCC = False
 
-
-    option = st.selectbox('Select optimization method', options = ('Number of Centers', 'Mean Distance', 'Radius and Population'))
+    option = st.selectbox('Select optimization method:', options = ('Number of Centers', 'Mean Distance', 'Radius and Population'))
 
        # explanation
     st.markdown(
         """
         <details>
-            <summary>Explanation</summary>
+            <summary>What does optimization method mean?</summary>
             You have several options to get the optimal locations for your needs:
             <ul>
                 <li><strong>Number of centers </strong><br>
@@ -195,77 +212,70 @@ def app():
                     'percent': percent_pop
                 }
             response1 = requests.get(base_url, params = params)
-            st.write(response1.status_code)
+            #st.write(response1.status_code)
             if response1.status_code == 500:
-                coordinate = [
-            [48.51070336,  9.05212022],
-            [49.95573197, 10.91326732],
-            [53.75704471, 10.06461158],
-            [51.17749919,  6.9799287 ],
-            [51.94432852, 10.19479602],
-            [52.47122199,  8.2674454 ],
-            [52.72249955, 13.29947277],
-            [48.34339277, 11.74843626]]
-                st.write('Internal server erros, using hardcoded coordinates instead.')
+                st.write('Internal server error.')
 
             else:
-                #response1 = response1.json()
-                coordinate = response1.json()["centers"]
+                response1 = response1.json()
+                avg_distance = response1["avg_distance"]
+                coordinate = response1["centers"]
+                pop_in_radius = response1.get('Population in Radius')
+                percent_in_radius = response1.get('Percent in Radius')
 
         with st.spinner('Generating Map'):
-            avg_distance = response1.json()["avg_distance"]
-            st.write(f'Number of centers: {len(coordinate)}')
-            st.write(f'Average distance to customers: {round(avg_distance, 2)} km')
-
+                col1, col2, col3, col4 = st.columns(4)
+                with col1:
+                    st.metric('Number of centers:', len(coordinate))
+                with col2:
+                    st.metric('Average distance to customers:', f'{round(avg_distance, 1)} Km')
+                if pop_in_radius != None:
+                    with col3:
+                        st.metric(f'Population in radius:', f'{round(pop_in_radius/1000000, 2)} M')
+                    with col4:
+                        st.metric(f'Percent of population in radius', f'{round(percent_in_radius*100, 1)}%')
             #Get Dataframe
-            url = 'https://nominatim.openstreetmap.org/reverse'
-            responses=[]
-            for coordinates in coordinate:
+                url = 'https://nominatim.openstreetmap.org/reverse'
+                responses=[]
+                for coordinates in coordinate:
 
-                lat = coordinates[0]
-                lon = coordinates[1]
-                params = {
+                    lat = coordinates[0]
+                    lon = coordinates[1]
+                    params = {
                         'lat': lat,
                         'lon': lon,
                         'format': 'json'
                     }
-                response = requests.get(url, params = params).json()
-                responses.append(response['display_name'])
-            df = pd.DataFrame({
-                "Address": responses,
-                "Coordinates": coordinate
-            }, index=list(range(1, (len(coordinate)+1))))
+                    response = requests.get(url, params = params).json()
+                    responses.append(response['display_name'])
+                df = pd.DataFrame({
+                    "Address": responses,
+                    "Coordinates": coordinate
+                }, index=list(range(1, (len(coordinate)+1))))
 
             #center the map
-            center =[sum(i[0] for i in coordinate)/len(coordinate), sum(i[1] for i in coordinate)/len(coordinate)]
+                center =[sum(i[0] for i in coordinate)/len(coordinate), sum(i[1] for i in coordinate)/len(coordinate)]
 
 
             #display map
-            col1, col2 = st.columns(2)
-            with col1:
-                m = folium.Map(location=center)
+                col1, col2 = st.columns(2)
+                with col1:
+                    m = folium.Map(location=center)
 
 
             #display coordinates on map and style tooltip
-            for i in range(len(df)):
-                folium.Marker(coordinate[i], tooltip=df.loc[i+1,'Address']).add_to(m)
+                for i in range(len(df)):
+                    folium.Marker(coordinate[i], tooltip=df.loc[i+1,'Address']).add_to(m)
                 # display circles if Radius is chosen
-                if radius != None:
-                    folium.Circle(location=coordinate[i], radius=radius*1000).add_to(m)
+                    if radius != None:
+                        folium.Circle(location=coordinate[i], radius=radius*1000).add_to(m)
 
             #Google Maps
-            basemaps = {
+                basemaps = {
                 'Google Maps': folium.TileLayer(
                     tiles = 'https://mt1.google.com/vt/lyrs=h&x={x}&y={y}&z={z}',
                     attr = 'Google',
                     name = 'Google Maps',
-                    overlay = True,
-                    control = True
-                ),
-                'Google Satellite': folium.TileLayer(
-                    tiles = 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
-                    attr = 'Google',
-                    name = 'Google Satellite',
                     overlay = True,
                     control = True
                 ),
@@ -282,71 +292,58 @@ def app():
                     name = 'Google Satellite',
                     overlay = True,
                     control = True
-                ),
-                'Esri Satellite': folium.TileLayer(
-                    tiles = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-                    attr = 'Esri',
-                    name = 'Esri Satellite',
-                    overlay = True,
-                    control = True
                 )
             }
 
-            ##Data for heatmap
-            #df_heat = df_small.drop(['Centers'], axis=1)
-            #df_heat = df_heat.to_numpy()
-            #HeatMap(df_heat).add_to(m)
-
-
             # Add custom basemaps
 
-            basemaps['Google Maps'].add_to(m)
-            basemaps['Google Satellite Hybrid'].add_to(m)
-            basemaps['Google Terrain'].add_to(m)
-            folium.LayerControl().add_to(m)
+                basemaps['Google Maps'].add_to(m)
+                basemaps['Google Satellite Hybrid'].add_to(m)
+                basemaps['Google Terrain'].add_to(m)
+                folium.LayerControl().add_to(m)
 
 
 
 
             #Plugins
 
-            plugins.Fullscreen().add_to(m)
-            plugins.LocateControl().add_to(m)
-            plugins.MeasureControl(position='topright', primary_length_unit='kilometers', secondary_length_unit='miles', primary_area_unit='sqmeters', secondary_area_unit='acres').add_to(m)
-            minimap = plugins.MiniMap()
-            m.add_child(minimap)
+                plugins.Fullscreen().add_to(m)
+                plugins.LocateControl().add_to(m)
+                plugins.MeasureControl(position='topright', primary_length_unit='kilometers', secondary_length_unit='miles', primary_area_unit='sqmeters', secondary_area_unit='acres').add_to(m)
+                minimap = plugins.MiniMap()
+                m.add_child(minimap)
 
             #####
 
 
             #automatic zoom start
 
-            sw = min(coordinate)[0], min(coordinate)[1]
-            ne = max(coordinate)[0], max(coordinate)[1]
+                sw = min(coordinate)[0], min(coordinate)[1]
+                ne = max(coordinate)[0], max(coordinate)[1]
 
-            m.fit_bounds([sw, ne])
+                m.fit_bounds([sw, ne])
 
 
 
 
             # call to render Folium map in Streamlit
 
-            folium_static(m)
+                folium_static(m)
 
             # get additional data
-            st.markdown('## Additional Data')
-            for c in country_list:
-                st.markdown(f"**{c}**")
-                try:
-                    st.dataframe(show_country_statistics(trans[c]))
-                except:
-                    st.write("No additional data found.")
+                st.markdown('## Additional Data')
+                for c in country_list:
+                    st.markdown(f"**{c}**")
+                    try:
+                        st.dataframe(show_country_statistics(trans[c]))
+                    except:
+                        st.write("No additional data found.")
 
             #Get Address to display on page
-            st.write("## Your Centers:")
-            st.dataframe(df)
-            csv = df.to_csv()
-            st.download_button(label = 'Download as CSV file', data = csv, file_name = 'centers.csv')
+                st.write("## Your Centers:")
+                st.dataframe(df)
+                csv = df.to_csv()
+                st.download_button(label = 'Download as CSV file', data = csv, file_name = 'centers.csv')
 
 
     # no button pressed
