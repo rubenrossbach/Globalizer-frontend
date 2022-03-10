32f1ec6d9d964a0845f867fb7707aaffe14bbc33
Martin Reichardt <mrlogin@mailbox.org>
2022-03-10 12:32:29 +0100

    Added Ease of doing business rank to the df and also rounded the CPIA business regulatory environment rating to only one digit after comma

--- country_statistics.py
+++ country_statistics.py
@@ -8,13 +8,16 @@ def show_country_statistics(iso):
         data = pd.read_excel(url, 0, skiprows=[0,1,2])
         data = data.drop(['Country Name', 'Country Code'], axis=1)
         data = data.set_index('Indicator Name')
-        data = data.loc[['Population, total', 'Urban population (% of total population)',\
+        data_ = data.loc[['Population, total', 'Urban population (% of total population)',\
                         'GDP (current US$)','GNI per capita (constant 2015 US$)',\
                         'Final consumption expenditure (constant 2015 US$)',\
                         'CPIA business regulatory environment rating (1=low to 6=high)',\
                         'Ease of doing business rank (1=most business-friendly regulations)'],\
                         ["2020"]]
-        data = data.rename(index={'Population, total': 'Population (in Millions)',\
+        business_rank = data.loc[['Ease of doing business rank (1=most business-friendly regulations)'],["2019"]]
+        business_rank = business_rank.rename(columns={"2019":"2020"})
+        data_ = pd.concat([data_, business_rank], ignore_index=False, axis=0)
+        data_ = data_.rename(index={'Population, total': 'Population (in Millions)',\
                                 'Urban population (% of total population)': 'Urban population (in %)',\
                                 'GDP (current US$)': 'GDP (in US$B)',\
                                 'GNI per capita (constant 2015 US$)': 'GNI per capita (in US$)',\
@@ -27,8 +30,8 @@ def show_country_statistics(iso):
         data.loc['GDP (in US$B)',''] = '{0:.2f}'.format(data.loc['GDP (in US$B)','']/1000000000)
         data.loc['GNI per capita (in US$)',''] = '{0:.2f}'.format(data.loc['GNI per capita (in US$)',''])
         data.loc['Final consumption expenditure (in US$B)',''] = '{0:.2f}'.format(data.loc['Final consumption expenditure (in US$B)','']/1000000000)
-        data.loc['CPIA business regulatory environment rating (1=low to 6=high)',''] = '{0:.2f}'.format(data.loc['CPIA business regulatory environment rating (1=low to 6=high)',''])
-        data.loc['Ease of doing business rank (1=most business-friendly regulations)',''] = '{0:.2f}'.format(data.loc['Ease of doing business rank (1=most business-friendly regulations)',''])
+        data.loc['CPIA business regulatory environment rating (1=low to 6=high)',''] = '{0:.1f}'.format(data.loc['CPIA business regulatory environment rating (1=low to 6=high)',''])
+        data.loc['Ease of doing business rank (1=most business-friendly regulations)',''] = '{0:.0f}'.format(data.loc['Ease of doing business rank (1=most business-friendly regulations)',''])
         data = data.dropna()
         data = data[data[''] != 'nan']
     except:
